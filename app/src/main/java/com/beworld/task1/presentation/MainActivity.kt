1f6efa1fb73728da2d115640bfc2372d9f6aa83c
beworld.pasha <beworld.pasha@yandex.ru>
2023-09-04 10:11:17 +0300

    View Binding added

--- app/src/main/java/com/beworld/task1/presentation/MainActivity.kt
+++ app/src/main/java/com/beworld/task1/presentation/MainActivity.kt
@@ -3,10 +3,13 @@ package com.beworld.task1.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.beworld.task1.R
+import com.beworld.task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
